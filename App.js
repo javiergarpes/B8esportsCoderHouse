28c61f3aac2a42e5de947f742a3f9530b7244af6
javiergarpes <javagagar29@gmail.com>
2023-08-18 19:24:31 -0400

    Terminado

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo de  B8esrts Coder :</Text>
+      <Text>Hola Mundo de  B8esports Coder :</Text>
       <StatusBar style="auto" />
     </View>
   );
